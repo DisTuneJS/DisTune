57910aecd52efef48b0881d169dd699693ae988b
Boofdev <hexahigh0@gmail.com>
2025-12-24 13:42:37 +0100

    fix(stream): remove shell option from FFmpeg check

--- src/core/DisTuneStream.ts
+++ src/core/DisTuneStream.ts
@@ -30,7 +30,7 @@ export const checkFFmpeg = (distune: DisTune) => {
   const debug = (str: string) => distune.emit(Events.FFMPEG_DEBUG, str);
   try {
     debug(`[test] spawn ffmpeg at '${path}' path`);
-    const process = spawnSync(path, ["-h"], { windowsHide: true, shell: true, encoding: "utf-8" });
+    const process = spawnSync(path, ["-h"], { windowsHide: true, encoding: "utf-8" });
     if (process.error) throw process.error;
     if (process.stderr && !process.stdout) throw new Error(process.stderr);
 
